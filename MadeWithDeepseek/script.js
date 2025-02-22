bff5df17278e7edcfa8d46341d4f78595f518970
cbgames-xyz <106137185+cbgames-xyz@users.noreply.github.com>
2025-02-23 00:05:18 +1300

    guess scrollable

--- MadeWithDeepseek/script.js
+++ MadeWithDeepseek/script.js
@@ -1,24 +1,27 @@
-    const output = document.getElementById('output');
-    let index = 0;
-    
-    function typeMessage(message, delay) {
-        return new Promise(resolve => {
-            let i = 0;
-            const interval = setInterval(() => {
-                if (i < message.length) {
-                    output.textContent += message[i];
-                    i++;
-                } else {
-                    clearInterval(interval);
-                    setTimeout(resolve, delay);
-                }
-            }, 140); 
-        });
+const output = document.getElementById('output');
+let index = 0;
+
+function typeMessage(message, delay) {
+    return new Promise(resolve => {
+        let i = 0;
+        const interval = setInterval(() => {
+            if (i < message.length) {
+                output.textContent += message[i];
+                i++;
+            } else {
+                clearInterval(interval);
+                setTimeout(resolve, delay);
+            }
+        }, 140); // Typing speed
+    });
+}
+
+async function simulateLoading() {
+    for (const message of messages) {
+        await typeMessage("> " + message.text + '\n', message.delay);
+        // Automatically scroll to the bottom after each message
+        output.parentElement.scrollTop = output.parentElement.scrollHeight;
     }
-    async function simulateLoading() {
-        for (const message of messages) {
-            await typeMessage("> " + message.text + '\n', message.delay); 
-        }
-    }
-    
-    simulateLoading();
+}
+
+simulateLoading();
