eb4333f40d3c71ce5a5674460a3ae0fd17d9cb2d
cbgames-xyz <106137185+cbgames-xyz@users.noreply.github.com>
2025-02-22 20:07:17 +1300

    ..

--- messages.js
+++ messages.js
@@ -4,6 +4,6 @@ const messages = [
     { text: "Starting services...", delay: 3000 },  
     { text: "System ready!", delay: 1000 },          
     { text: "Welcome to the terminal.", delay: 500 }, 
-    { text: "Waiting for Input", delay: 700},
+    { text: "Waiting for Input..", delay: 700},
     { text: ".....................................................................", delay: 2000},
 ];
